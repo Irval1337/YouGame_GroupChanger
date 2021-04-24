085eb90272696ff6f6f13f21b86ff8eaecaa0f38
Irval <56792892+Irval1337@users.noreply.github.com>
2021-04-24 10:31:03 +0300

    Update styles.js

--- styles.js
+++ styles.js
@@ -30,6 +30,35 @@ var styles = `
       box-shadow: 0 1 6px #00fff2;
     }
   }
+
+.tracking-in-expand {
+  -webkit-animation: tracking-in-expand 3s cubic-bezier(0.215, 0.610, 0.355, 1.000) infinite alternate backwards;
+          animation: tracking-in-expand 3s cubic-bezier(0.215, 0.610, 0.355, 1.000) infinite alternate backwards;
+}
+@-webkit-keyframes tracking-in-expand {
+  0% {
+    letter-spacing: -0.5em;
+    opacity: 0;
+  }
+  40% {
+    opacity: 0.6;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+@keyframes tracking-in-expand {
+  0% {
+    letter-spacing: -0.5em;
+    opacity: 0;
+  }
+  40% {
+    opacity: 0.6;
+  }
+  100% {
+    opacity: 1;
+  }
+}
 `;
 var styleSheet = document.createElement("style");
 styleSheet.innerText = styles;
